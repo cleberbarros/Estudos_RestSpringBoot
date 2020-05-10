3e8da0f5b06d6a45a316e28abea8e4e3c5230e15
cleberbarros <cleberbarros.ti@gmail.com>
2020-05-10 12:30:43 -0300

    Implementacão do controle de acesso aos metodos do LancamentoResource

--- Curso_Spring_Boot_Angular/algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ Curso_Spring_Boot_Angular/algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -17,6 +17,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -54,11 +55,13 @@ public class LancamentoResource {
 	
 	// Pageable para fazer a paginação
 	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO')and #oauth2.hasScope('read')")
 	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable){
 		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
 	}
 	
 	@GetMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO')and #oauth2.hasScope('read')")
 	public ResponseEntity<Lancamento> buscar(@PathVariable Long codigo) {
 		
 	Optional<Lancamento> lancamentoPesquisado = lancamentoRepository.findById(codigo);
@@ -80,6 +83,7 @@ public class LancamentoResource {
 	}
 	
 	@PostMapping
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO')and #oauth2.hasScope('write')")
 	public ResponseEntity<Lancamento> criar (@Valid @RequestBody Lancamento lancamento, HttpServletResponse response){
 	  Lancamento lancamentoSalvo =lancamentoService.salvar(lancamento);
 	  
@@ -91,6 +95,7 @@ public class LancamentoResource {
 	
 	@DeleteMapping("/{codigo}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO')and #oauth2.hasScope('write')")
 	public void excluir(@PathVariable Long codigo ){
 		
 		lancamentoRepository.deleteById(codigo);
